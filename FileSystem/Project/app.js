7323039722cdc1832f885d40c7b14da1cc8f238d
Nitish <nitishsingh@midknightstudio.com>
2025-03-09 13:55:18 +0530

    Added File system

--- FileSystem/Project/app.js
+++ FileSystem/Project/app.js
@@ -1,12 +1,23 @@
 const fs = require("fs/promises");
 
 (async () => {
+
+    const commandFileHandler = await fs.open("./command.txt", "r")
+
     const watcher = fs.watch("./command.txt");
 
     for await(const event of watcher)  {
         if (event.eventType === "change") {
             // the file was changed...
             console.log("The file was changed");
+            
+            // get the size of file
+            const size = (await commandFileHandler.stat()).size;
+            console.log(size)
+            
+            // we want to read a content
+            const content = await commandFileHandler.read(Buffer.alloc(size));
+            console.log(content);
         }
     }
 })();
