ecfd9d461e44f5e26a9c25dadc017de97ccfc98d
Nitesh <niti2002sh@gmail.com>
2025-03-16 22:21:50 +0530

    Make code clean in FileSystem Project

--- FileSystem/Project/app.js
+++ FileSystem/Project/app.js
@@ -4,23 +4,29 @@ const fs = require("fs/promises");
 
     const commandFileHandler = await fs.open("./command.txt", "r")
 
+    commandFileHandler.on("change", async () => {
+        // get the size of file
+        const size = (await commandFileHandler.stat()).size;
+        // allocate our buffer with the size of the file
+        const buff = Buffer.alloc(size);
+        // the location at which we want to start filling our buffer
+        const offset = 0;
+        // how many bytes we want to read
+        const length = buff.byteLength;
+        // the position that we want to start reading the file from
+        const position = 0;;
+        
+        // we always want to read the whole content (from beginning all the way to the end)
+        const content = await commandFileHandler.read(buff, offset, length, position);
+        console.log(content);
+    })
+
+    // watcher...
     const watcher = fs.watch("./command.txt");
 
     for await(const event of watcher)  {
         if (event.eventType === "change") {
-            // the file was changed...
-            console.log("The file was changed");
-            
-            // get the size of file
-            const size = (await commandFileHandler.stat()).size;
-            const buff = Buffer.alloc(size);
-            const offset = 0;
-            const length = buff.byteLength;
-            const position = 0;;
-            
-            // we want to read a content
-            const content = await commandFileHandler.read(buff, offset, length, position);
-            console.log(content);
+            commandFileHandler.emit("change");
         }
     }
 })();
