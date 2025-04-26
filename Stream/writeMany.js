09850b84c12bdb44bb9ed91292d0d0fe2645d8f3
Nitesh <niti2002sh@gmail.com>
2025-04-26 22:00:09 +0530

    Starting of stream

--- Stream/writeMany.js
+++ Stream/writeMany.js
@@ -11,12 +11,27 @@
 // })()
 
 
-const fs = require("fs");
-console.time("writeData");
-fs.open("test.txt", "w", (err, fd) => {
-    for(let i = 0; i < 1000000; i++) {
+// const fs = require("fs");
+
+// console.time("writeData");
+// fs.open("test.txt", "w", (err, fd) => {
+//     for(let i = 0; i < 1000000; i++) {
+//         const buff = Buffer.from(` ${i} `, "utf-8");
+//         fs.writeSync(fd, buff);
+//     }
+//     console.timeEnd("writeData");
+// })
+
+// const fs = require("fs/promises");
+
+(async () => {
+    console.time("writeMany");
+    const fileOpen = await fs.open("test.txt", "w");
+    const stream = fileOpen.createWriteStream();
+    for(let i = 0; i < 1000000; i++){
         const buff = Buffer.from(` ${i} `, "utf-8");
-        fs.writeSync(fd, buff);
+        stream.write(buff);
     }
-    console.timeEnd("writeData");
-})
+    fileOpen.close();
+    console.timeEnd("writeMany");
+})()
