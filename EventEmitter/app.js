b61e0f257808ec6672037d7c60bd99b33ccb45dc
Nitesh <niti2002sh@gmail.com>
2025-05-05 23:13:54 +0530

    Added a read stream

--- EventEmitter/app.js
+++ EventEmitter/app.js
@@ -1,41 +1,41 @@
-const EventEmitter = require('events');
+// const EventEmitter = require('events');
 
-class Emitter extends EventEmitter {}
+// class Emitter extends EventEmitter {}
 
-const myE = new Emitter();
+// const myE = new Emitter();
 
-myE.on('foo', () => {
-    console.log('An event occurred!'); 
-})
+// myE.on('foo', () => {
+//     console.log('An event occurred!'); 
+// })
 
-myE.on('foo', (x) => {
-    console.log('An event occurred!', x); 
-})
+// myE.on('foo', (x) => {
+//     console.log('An event occurred!', x); 
+// })
 
-myE.on('event', () => {
-    console.log('An event occurred2!'); 
-})
+// myE.on('event', () => {
+//     console.log('An event occurred2!'); 
+// })
 
-myE.emit('foo');
-myE.emit('foo', 'done');
-myE.emit('event')
+// myE.emit('foo');
+// myE.emit('foo', 'done');
+// myE.emit('event')
 
 
-// console.log('A');
+console.log('A');
 
-// setTimeout(() => {
-    // for(let i=0; i<90000000000; i++){};
-//     console.log('C');
+setTimeout(() => {
+    for(let i=0; i<1; i++){};
+    console.log('C');
     
-// }, 1)
+}, 1)
 
-// function add(a, b){
-//     return a + b;
-// }
+function add(a, b){
+    return a + b;
+}
 
-// function main(a, b, callback){
-//     let c = callback(a, b);
-//     console.log(c);
-// }
+function main(a, b, callback){
+    let c = callback(a, b);
+    console.log(c);
+}
 
-// main(1, 2, add);
+main(1, 2, add);
