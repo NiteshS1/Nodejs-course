9072ca041dcb8ab70b187bd8048d107d0d220e83
Nitesh <niti2002sh@gmail.com>
2025-05-08 15:18:41 +0530

    Added a read stream for a filter

--- Stream/read-big/readBig.js
+++ Stream/read-big/readBig.js
@@ -2,7 +2,7 @@ const fs = require("node:fs/promises");
 
 (async () => {
   console.time("readBig");
-  const fileHandleRead = await fs.open("text-gigantic.txt", "r");
+  const fileHandleRead = await fs.open("text-small.txt", "r");
   const fileHandleWrite = await fs.open("dest.txt", "w");
 
   const streamRead = fileHandleRead.createReadStream({
@@ -10,17 +10,41 @@ const fs = require("node:fs/promises");
   });
   const streamWrite = fileHandleWrite.createWriteStream();
 
+  let split = "";
+
   streamRead.on("data", (chunk) => {
     const numbers = chunk.toString("utf-8").split("  ");
 
-    console.log(numbers);
-
-    if (!streamWrite.write(chunk)) {
-      streamRead.pause();
+    if (Number(numbers[0]) !== Number(numbers[1]) - 1) {
+      if (split) {
+        numbers[0] = split.trim() + numbers[0].trim();
+      }
     }
+
+    if (
+      Number(numbers[numbers.length - 2]) + 1 !==
+      Number(numbers[numbers.length - 1])
+    ) {
+      split = numbers.pop();
+    }
+
+    numbers.forEach((number) => {
+      let n = Number(number);
+
+      if (n % 10 === 0) {
+        if (!streamWrite.write(" " + n + " ")) {
+          streamRead.pause();
+        }
+      }
+    });
   });
 
   streamWrite.on("drain", () => {
     streamRead.resume();
+  });
+
+  streamRead.on("end", () => {
+    console.log("Done reading.");
+    console.timeEnd("readBig");
   });
 })();
