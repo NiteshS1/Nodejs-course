ab3da930845495b1f7dd3406afe0df2c87c106ab
Nitesh <niti2002sh@gmail.com>
2025-05-15 22:31:24 +0530

    Added a read stream Piping

--- Stream/copy/copy.js
+++ Stream/copy/copy.js
@@ -1,3 +1,4 @@
+const { pipeline } = require("node:stream");
 const fs = require("node:fs/promises");
 
 // File Size Copied: 1 GB
@@ -18,29 +19,68 @@ const fs = require("node:fs/promises");
 // Memory Usage: 30 MB
 // Execution Time: 2 s
 // Maximum File Size Able to Copy: No Limit
+// (async () => {
+//   console.time("copy");
+
+//   const srcFile = await fs.open("text-gigantic.txt", "r");
+//   const destFile = await fs.open("text-copy.txt", "w");
+
+//   let bytesRead = -1;
+
+//   while (bytesRead !== 0) {
+//     const readResult = await srcFile.read();
+//     bytesRead = readResult.bytesRead;
+
+//     if (bytesRead !== readResult.buffer.length) {
+//       // we have some null bytes, remove them at the end of the returned buffer
+//       // and then write to our file
+//       const indexOfNotFilled = readResult.buffer.indexOf(0);
+//       const newBuffer = Buffer.alloc(indexOfNotFilled);
+//       readResult.buffer.copy(newBuffer, 0, 0, indexOfNotFilled);
+//       destFile.write(newBuffer);
+//     } else {
+//       destFile.write(readResult.buffer);
+//     }
+//   }
+
+//   console.timeEnd("copy");
+// })();
+
+// File Size Copied: 1 GB
+// Memory Usage: 30 MB
+// Execution Time: 1 s
+// Maximum File Size Able to Copy: No Limit
 (async () => {
   console.time("copy");
 
-  const srcFile = await fs.open("text-gigantic.txt", "r");
+  const srcFile = await fs.open("text-big.txt", "r");
   const destFile = await fs.open("text-copy.txt", "w");
 
-  let bytesRead = -1;
+  const readStream = srcFile.createReadStream();
+  const writeStream = destFile.createWriteStream();
 
-  while (bytesRead !== 0) {
-    const readResult = await srcFile.read();
-    bytesRead = readResult.bytesRead;
+  // console.log(readStream.readableFlowing);
 
-    if (bytesRead !== readResult.buffer.length) {
-      // we have some null bytes, remove them at the end of the returned buffer
-      // and then write to our file
-      const indexOfNotFilled = readResult.buffer.indexOf(0);
-      const newBuffer = Buffer.alloc(indexOfNotFilled);
-      readResult.buffer.copy(newBuffer, 0, 0, indexOfNotFilled);
-      destFile.write(newBuffer);
-    } else {
-      destFile.write(readResult.buffer);
-    }
-  }
+  // readStream.pipe(writeStream);
 
-  console.timeEnd("copy");
+  // console.log(readStream.readableFlowing);
+
+  // readStream.unpipe(writeStream);
+
+  // console.log(readStream.readableFlowing);
+
+  // readStream.pipe(writeStream);
+
+  // console.log(readStream.readableFlowing);
+
+  // readStream.on("end", () => {
+  //   console.timeEnd("copy");
+  // });
+
+  // Don't use pipe in production, use pipeline instead! It will automatically
+  // handle the cleanings for you and give you an easy way for error handling
+  pipeline(readStream, writeStream, (err) => {
+    console.log(err);
+    console.timeEnd("copy");
+  });
 })();
